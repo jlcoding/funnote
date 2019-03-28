38ba54be3382dbf672c03bea6d8c068530a8d04e
jl <41369927@qq.com>
2019-03-29 04:43:33 +0800

    base file control

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const Note = require("./models/notes").Note
+const Note = require("./models/notes");
 
 function list() {
     let note = new Note();
-    console.log(note.list());
+    let lists = note.list();
+    var html = '';
+    
+    lists.forEach(function(e) {
+        var template = '<li onclick="detail(\'' + e.id + '\')">' + e.title + '</li>';
+        html += template;
+    });
+    var list = document.getElementById('list');    
+    list.innerHTML = html;
 }
 
-function detail() {
+function detail(id) {
     let note = new Note();
-    console.log(note.detail(document.getElementById("id").value));
+    let noteDetail = note.detail(id);
+    document.getElementById("id").value = id;
+    document.getElementById("title").value = noteDetail.title;
+    document.getElementById("content").value = noteDetail.content;
 }
 
 function saveNote() {
-    let note = new Note();
-    let id = note.save(document.getElementById("title").value, document.getElementById("content").value);
+    let id = document.getElementById("id").value;
+    let title = document.getElementById("title").value;
+    let content = document.getElementById("content").value;
+    id = new Note(id, title, content).save();
     console.log(id);
+    list();
 }
+
+function del() {
+    let id = document.getElementById("id").value;
+    new Note(id, null, null).del();
+    list();
+}
+
--- models/notes.js
+++ models/notes.js
@@ -1,39 +1,62 @@
-const Persist = require("./../core/persist").Persist;
+const Persist = require("./../core/persist");
+const cacheManager = require("./../core/cache")
 const uuid = require("uuid");
 
 /**
  * 笔记实体
  */
 class Note {
-  constructor() {  
-    this.id = -1
-    this.title = '';
-    this.content = '';
+
+  constructor(id, title, content) {  
+    this.id = id
+    this.title = title;
+    this.content = content;
     this.createTime = -1;
   }
 
-  save(title, content) {
-    this.id = uuid.v1();
-    this.title = title;
-    this.content = content;
+  save() {
+    if(!this.id) {
+      this.id = uuid.v1();
+    }
     this.createTime = (new Date() / 1);
     let persist = new Persist();
+    console.log(this);
     persist.saveData(this.id, this);
+    cacheManager.remove(this.id);
     return this.id;
   }
 
   detail(id) {
     let persist = new Persist();
-    return persist.readData(id);
+    let data = cacheManager.get(id);
+    if(!data) {
+      data = persist.readData(id);
+      if(!data) {
+        cacheManager.set(id, data);
+      }
+    }
+    return data;
   }
 
   list() {
     let persist = new Persist();
-    return persist.list();
+    var lists = persist.list();
+    let notes = Array();
+    let _self = this;
+    lists.forEach(function(e) {
+      let note = _self.detail(e);
+      console.log(note);
+      notes.push(note);
+      cacheManager.set(note.id, note);
+    });
+    return notes;
+  }
 
+  del() {
+    let persist = new Persist();
+    persist.del(this.id);
+    cacheManager.remove(this.id);
   }
 }
 
-
-
-exports.Note = Note;
+module.exports = Note;
